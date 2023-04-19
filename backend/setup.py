e9da13900c6cfab75f709b80db3a957ce8d6e407
Vijayasai Sanagaram <vijayasai10@gmail.com>
2023-04-19 10:31:16 +0530

    Updated the URL of the postgres and the host to use in the app.run()

--- backend/setup.py
+++ backend/setup.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:root@localhost:5432/PharmaShare'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://pharmaadmin:admin$4ss@pharmashare.postgres.database.azure.com:5432/pharmashare'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 cors = CORS(app, supports_credentials=True)
